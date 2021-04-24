499be2d4c398abcf9208d91baf62144603f1b487
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-24 11:46:07 +0200

    minor edit

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+import exif_writer
+
+ORIGINAL_FILES_PATH = ''
+COMPRESSED_FILES_PATH = ''
+
+if __name__ == '__main__':
+    exif_writer .edit_metadata(original_files_path=ORIGINAL_FILES_PATH, compressed_files_path=COMPRESSED_FILES_PATH)    
--- preferences.py
+++ preferences.py
@@ -1,5 +1,5 @@
-EXIFTOOL_PATH = "/usr/local/bin/exiftool"
+EXIFTOOL_PATH = '/usr/local/bin/exiftool'
 
-ORIGINAL_FILE_EXTENSIONS = ["mp4", "mov"]
-COMPRESSED_FILE_EXTENSIONS = ["mp4", "mov"]  # Compressed file extensions to look for
-COMPRESSED_MARKER = " (HEVC)"  # Extra text marker in compressed filename. Usage: FILENAME + COMPRESSED_MARKER + EXTENSION (example: "IMG_009.MP4" >> "IMG_009 (HEVC).MOV")
+ORIGINAL_FILE_EXTENSIONS = ['mp4', 'mov']
+COMPRESSED_FILE_EXTENSIONS = ['mp4', 'mov']  # Compressed file extensions to look for
+COMPRESSED_MARKER = ' (HEVC)'  # Extra text marker in compressed filename. Usage: FILENAME + COMPRESSED_MARKER + EXTENSION (example: "IMG_009.MP4" >> "IMG_009 (HEVC).MOV")
